1dfcc546545f2b7e650145b6eca2c6937f0ad930
filbert42 <m.d.filbert@gmail.com>
2022-01-29 19:01:40 +0200

    Change path to dish variants json to be in the same dir as binary

--- src/utils.rs
+++ src/utils.rs
@@ -6,7 +6,16 @@ pub fn choose_random_dish(variants: &[Dish]) -> Option<&Dish> {
 }
 
 pub fn get_dish_variants() -> Vec<Dish> {
-    let raw_json = std::fs::read_to_string("./src/data/dishes_list.json")
+    // set executable path as current working directory
+    let current_dir = std::env::current_exe()
+        .unwrap()
+        .as_path()
+        .parent()
+        .unwrap()
+        .to_owned();
+    std::env::set_current_dir(&current_dir).unwrap();
+
+    let raw_json = std::fs::read_to_string("./data/dishes_list.json")
         .expect("Something went wrong reading the file");
     let list: Vec<Dish> =
         serde_json::from_str(&raw_json).unwrap_or_else(|_| vec![Dish::new("", "", vec![""])]);
